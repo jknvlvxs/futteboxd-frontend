9debacce1d5ffc34b1cba1dc97e2d2505f36ea0e
jknvlvxs <dev.julio.alves@gmail.com>
2023-12-14 21:50:39 -0300

    feature: add default to league scores

--- components/LeagueScores.js
+++ components/LeagueScores.js
@@ -11,7 +11,7 @@ export default function LeagueScores(props) {
     <section>
       <div className="max-w-7xl mx-auto w-full">
         <div className="my-20 space-y-10 text-center sm:my-16 md:space-y-14">
-          <div className="space-y-1 md:space-y-8">
+          <div className="space-y-1 md:space-y-4">
             <div class="flex items-center justify-center">
               <img
                 src={league.logo}
@@ -31,52 +31,63 @@ export default function LeagueScores(props) {
               />
               <h1 class="text-xl font-bold" children={league.country} />
             </div>
+            <div class="flex items-center justify-center">
+              <h1 class="text-xl font-bold" children={league.season} />
+            </div>
           </div>
         </div>
       </div>
       <div className="mt-10 space-y-6 text-center mb-16 md:mt-16">
-        <div className="w-5/6 p-6 mx-auto bg-gray-100 rounded-2xl bg-opacity-70 md:bg-opacity-100 md:p-10">
-          {/* <CountriesSelect values={countries} onChange={onChangeCountry} /> */}
-          {/* <SeasonsSelect values={seasons} onChange={onChangeSeason} /> */}
-          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 content-center">
-            {league.scores.map((match) => (
-              <a
-                key={match.id}
-                href={`/scores/${match.id}`}
-                class="flex flex-row items-center justify-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-              >
-                <div class="flex flex-col items-center h-auto">
-                  <img
-                    class="object-cover w-12 md:w-16 rounded-t-lg m-2 rounded-none rounded-s-lg"
-                    src={match.homeTeam?.logo}
-                    alt=""
-                  />
-                  <p class="mb-1">{match.homeTeam.code}</p>
-                </div>
-
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {match.final_score || match.halftime_score}
-                  </h5>
-                  {match.penalty !== "-" && (
-                    <h5
-                      class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
-                      children={`(${match.penalty})`}
+        {league.scores.length > 0 ? (
+          <div className="w-5/6 p-6 mx-auto bg-gray-100 rounded-2xl bg-opacity-70 md:bg-opacity-100 md:p-10">
+            {/* <CountriesSelect values={countries} onChange={onChangeCountry} /> */}
+            {/* <SeasonsSelect values={seasons} onChange={onChangeSeason} /> */}
+            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 content-center">
+              {league.scores.map((match) => (
+                <a
+                  key={match.id}
+                  href={`/scores/${match.id}`}
+                  class="flex flex-row items-center justify-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+                >
+                  <div class="flex flex-col items-center h-auto">
+                    <img
+                      class="object-cover w-12 md:w-16 rounded-t-lg m-2 rounded-none rounded-s-lg"
+                      src={match.homeTeam?.logo || league.logo}
+                      alt=""
                     />
-                  )}
-                </div>
-                <div class="flex flex-col items-center h-auto">
-                  <img
-                    class="object-cover w-12 md:w-16 rounded-t-lg m-2 rounded-none rounded-s-lg"
-                    src={match.awayTeam?.logo}
-                    alt=""
-                  />
-                  <p class="mb-1">{match.awayTeam.code}</p>
-                </div>
-              </a>
-            ))}
+                    <p class="mb-1">{match.homeTeam.code || match.homeTeam}</p>
+                  </div>
+
+                  <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {match.final_score || match.halftime_score}
+                    </h5>
+                    {match.penalty !== "-" && (
+                      <h5
+                        class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
+                        children={`(${match.penalty})`}
+                      />
+                    )}
+                  </div>
+                  <div class="flex flex-col items-center h-auto">
+                    <img
+                      class="object-cover w-12 md:w-16 rounded-t-lg m-2 rounded-none rounded-s-lg"
+                      src={match.awayTeam?.logo || league.logo}
+                      alt=""
+                    />
+                    <p class="mb-1">{match.awayTeam.code || match.awayTeam}</p>
+                  </div>
+                </a>
+              ))}
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="w-5/6 p-6 mx-auto bg-gray-100 rounded-2xl bg-opacity-70 md:bg-opacity-100 md:p-10 h-52">
+            <h1>
+              Não foi encontrada nenhuma partida para esta liga na temporada :(
+            </h1>
+          </div>
+        )}
       </div>
     </section>
   );
